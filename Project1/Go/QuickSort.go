8f49ee25bd22ddaa9a13e27f84f312139516918e
Abdelrahman <mbode167@gmail.com>
2026-02-18 11:23:26 -0600

    Quick Sort in Go

---
+++ Project1/Go/QuickSort.go
@@ -0,0 +1,44 @@
+/*
+ * Name: Abdelrahman Ahmed
+ * Professor: Dr. Dai
+ * Course: CS416 - Software Engineering
+ * Date: Feb 17, 2026
+ *
+ * File: QuickSort.go
+ * Description:
+ *   Implements Quick Sort, a divide-and-conquer algorithm that partitions the array around a pivot and sorts subranges.
+ *   Average time is O(n log n), but worst-case can be O(n^2) depending on pivot choices.
+ */
+
+package main
+
+// QuickSort sorts the slice in ascending order (in-place).
+func QuickSort(a []int) {
+	if len(a) <= 1 {
+		return
+	}
+	quickSort(a, 0, len(a)-1)
+}
+
+func quickSort(a []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	p := partitionLomuto(a, lo, hi)
+	quickSort(a, lo, p-1)
+	quickSort(a, p+1, hi)
+}
+
+// partitionLomuto uses the last element as pivot (simple and common).
+func partitionLomuto(a []int, lo, hi int) int {
+	pivot := a[hi]
+	i := lo
+	for j := lo; j < hi; j++ {
+		if a[j] < pivot {
+			a[i], a[j] = a[j], a[i]
+			i++
+		}
+	}
+	a[i], a[hi] = a[hi], a[i]
+	return i
+}
