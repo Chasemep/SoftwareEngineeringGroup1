dfbdb6ffc2f56ebc6d3c3983869a2454aa39cbd2
alex-b005 <lexman2002@gmail.com>
2026-02-19 21:33:13 -0600

    Selection Sort Python

--- Project1/Python/SelectionSort.py
+++ Project1/Python/SelectionSort.py
@@ -1,10 +1,21 @@
+"""
+ Name: Alex Bryant
+ ID: 2
+ Professor: Dr. Dai
+ Course: CS416 - Software Engineering
+ Date: Feb 19, 2026
+
+ File: SelectionSort.py
+
+ """
+
 def selection_sort(arr):
-	for i in range(len(arr)):
-		min_idx = i
-		for j in range(i+1, len(arr)):
-			if arr[j] < arr[min_idx]:
-				min_idx = j
-		arr[i], arr[min_idx] = arr[min_idx], arr[i]
-	return arr
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
 
 print(selection_sort([64, 34, 25, 12, 22, 11, 90]))
