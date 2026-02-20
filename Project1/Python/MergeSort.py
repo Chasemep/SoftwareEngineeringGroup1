ac8e51d8d95e089c122b00c98c0e9d64c34c6c73
alex-b005 <lexman2002@gmail.com>
2026-02-19 21:12:55 -0600

    Updated Merge Sort

--- Project1/Python/MergeSort.py
+++ Project1/Python/MergeSort.py
@@ -1,3 +1,14 @@
+"""
+ Name: Alex Bryant
+ ID: 2
+ Professor Dr. Dai
+ Course: CS416 - Software Engineering
+ Date: Feb 19, 2026
+
+ File: MergeSort.py
+
+"""
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
