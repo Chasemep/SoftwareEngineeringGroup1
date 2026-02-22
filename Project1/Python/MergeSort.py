c280e4da14806bd651cc2e64614a30acf6ac9293
alex-b005 <lexman2002@gmail.com>
2026-02-22 12:33:39 -0600

    Python Merge Sort

--- Project1/Python/MergeSort.py
+++ Project1/Python/MergeSort.py
@@ -1,22 +1,40 @@
 """
- Name: Alex Bryant
- ID: 2
- Professor Dr. Dai
- Course: CS416 - Software Engineering
- Date: Feb 19, 2026
+Name: Alex Bryant
+ID: 2
+Professor: Dr. Dai
+Course: CS416 - Software Engineering
+Date: Feb 19, 2026
 
- File: MergeSort.py
-
+File: MergeSort.py
 """
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = key
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
     return arr
 
-print(insertion_sort([64, 34, 25, 12, 22, 11, 90]))
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Sample Input: ", arr)
+merge_sort(arr)
+print("Sample Output: ", arr)
