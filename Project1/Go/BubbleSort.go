31240728afb3960a8de09b7e5163b2e01520545b
Abdelrahman22-web <mbode167@gmail.com>
2026-02-23 14:32:12 -0600

    Go bubble sort (#47)
    * Bubble Sort in GO

---
+++ Project1/Go/BubbleSort.go
@@ -0,0 +1,38 @@
+/*
+ * Name: Abdelrahman Ahmed
+ * Professor: Dr. Dai
+ * Course: CS416 - Software Engineering
+ * Date: Feb 17, 2026
+ *
+ * File: BubbleSort.go
+ * Description:
+ *   Implements Bubble Sort, a simple comparison-based algorithm that repeatedly swaps adjacent out-of-order elements.
+ *   It runs in O(n^2) time in the worst case and is mainly useful for learning or very small inputs.
+ */
+
+package main
+
+import "fmt"
+
+// BubbleSort sorts the slice in ascending order (in-place).
+func BubbleSort(a []int) {
+	n := len(a)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-1-i; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			return
+		}
+	}
+}
+func main() {
+	test := []int{9, 3, 5, 1, 8, 2, 7, 4, 6}
+	fmt.Println("Before:", test)
+	BubbleSort(test)
+	fmt.Println("After: ", test)
+}
